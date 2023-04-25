6f7919c410548371f5f8ea2d1110e419138b3f91
worashf <uppert83@gmail.com>
2023-04-25 07:38:00 +0300

    Update dashboard

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,4 +1,18 @@
+import { Box, Icon, IconButton, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import Header from "../../components/header/Header";
 const Dashboard = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return <></>;
 };
 
