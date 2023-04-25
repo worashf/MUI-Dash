c6dec76b4aeddfe0378d6e7d631742ab30acbb53
Worash <uppert83@gmail.com>
2023-04-25 18:11:31 +0300

    Add team route

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
 import { useMode, ColorModeContext } from "./theme";
 import TopBar from "./layouts/TopBar";
 import Dashboard from "./pages/dashboard";
+import SideBar from "./layouts/Sidebar";
+import Team from "./pages/team";
 function App() {
   const [theme, colorMode] = useMode();
-
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <SideBar isSidebar={isSidebar} />
           <main className="content">
-            <TopBar />
+            <TopBar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
             </Routes>
           </main>
         </div>
