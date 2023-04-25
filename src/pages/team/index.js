a0e33a79aae9c57ffb6146017ad6d05e18413668
worashf <uppert83@gmail.com>
2023-04-25 07:36:31 +0300

    Add Team page

---
+++ src/pages/team/index.js
@@ -0,0 +1,27 @@
+import { Box, Icon, IconButton, Typography, useTheme } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
+import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
+import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import { tokens } from "../../theme";
+import { mockDataTeam } from "../../data/mockData";
+import Header from "../../components/header/Header";
+const Team = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box>
+      <Header title="Team" subTitle="Managing the Team Members" />
+
+      <Box>
+        <DataGrid />
+      </Box>
+    </Box>
+  );
+};
+
+export default Team;
